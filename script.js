7531f0986418d71bef7c94df8ce71075bcc291af
Alexander.German <questlogilt@mail.ru>
2020-03-28 17:18:45 +0300

    сейв 2

---
+++ script.js
@@ -0,0 +1,23 @@
+    let money = 10000;
+    let income = " продажа собранных бутылок ";
+    let addExpenses = "sex, drugs and Rock&Roll";
+    let misson = 250000;
+    let period = 6;
+    let deposit = 5;
+
+        console.log(typeof money);
+        console.log(typeof income);
+        console.log(Boolean(deposit));
+        
+
+            console.log(addExpenses.length);
+        
+                console.log("Период равен " + period + " месяцев");
+
+                    console.log(addExpenses.toLowerCase().split(" "));
+
+                        let budgetDay = money / 30;
+                        console.log(budgetDay);
+                   
+            
+    
