17791900931e6fe82694a707ba71a46d4b319fad
Alexander.German <questlogilt@mail.ru>
2020-04-07 14:47:58 +0300

    perviy commit

---
+++ game.js
@@ -0,0 +1,49 @@
+'use strict';
+
+alert("Угадай число от 1 до 100");
+
+const isNum = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+function randomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const gameRandom = function(){
+
+    const targetNumber = randomNum(1,100);
+    console.log(targetNumber);
+debugger;
+    return function checkNumber(){
+        const userNumber = prompt('Угадай число от 1 до 100');
+
+
+        if(isNum(userNumber)){
+            if(userNumber > targetNumber){
+                alert("Загаданное число больше");
+                return checkNumber();
+            }
+            else if(userNumber < targetNumber){
+                alert("Загаданное число меньше");
+                return checkNumber();
+            }
+            else if(userNumber !== null){
+                alert("Введи число!");
+                return checkNumber;
+            }
+            else if(userNumber === targetNumber){
+                alert("Поздравляю ты победил.");
+            }
+            else {
+                alert('Game over');
+            }
+        }
+    }
+
+};
+
+gameRandom()();
