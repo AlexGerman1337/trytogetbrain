9564c14cbcc54dccf1bd88fd6c9bb170855d480e
Alexander.German <questlogilt@mail.ru>
2020-04-13 02:44:44 +0300

    первый коммит

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+
+
+
+ let a = document.getElementById('start'), //a
+
+     b = document.getElementsByTagName('button')[0], //b
+     b1 = document.getElementsByTagName('button')[1], 
+
+     c = document.querySelector('#deposit-check'), // c
+
+     d = document.querySelectorAll('.additional_income'), //d
+
+
+     e0 = document.querySelector('.result-budget_day'), //e
+     e1 = document.querySelector('.result-expenses_month'),
+     e2 = document.querySelector('.result-additional_income'),
+     e3 = document.querySelector('.result-additional_expenses'),
+     e4 = document.querySelector('.result-income_period'),
+     e5 = document.querySelector('.result-target_month'),
+     e6 = document.querySelector('.result-budget_day'),
+     e7 = document.querySelector('.result-budget_day'),
+
+     f0 = document.querySelector('.main'),
+     f1 = document.querySelectorAll('.salary'),
+     f2 = document.querySelector('.salary-amount'),
+     f3 = document.querySelectorAll('.income');
+    //  f2 = document.querySelector('.'),
+    //  f3 = document.querySelector('.'),
+    //  f4 = document.querySelector('.'),
+    //  f5 = document.querySelector('.'),
+    //  f6 = document.querySelector('.'),
+    //  f7 = document.querySelector('.'),
+    //  f8 = document.querySelector('.'),
+    //  f9 = document.querySelector('.'),
+    //  f10 = document.querySelector('.'),
+    //  f11 = document.querySelector('.'),
+    //  f12 = document.querySelector('.');
+     console.log(f1);
+     
+     
+//  let z = console.log(a, b1, c, d, e0, e1, e2, e3, e4, e5, e6, e7);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задание по проекту, получить каждый элемент в отдельную переменную:
+
+
+
+// Кнопку "Рассчитать" через id
+
+// Кнопки “+” (плюс) через Tag, каждую в своей переменной. 
+
+// Чекбокс по id через querySelector
+
+// Поля для ввода возможных доходов (additional_income-item) при помощи querySelectorAll
+
+// // Каждый элемент в правой части программы через класс, 
+// которые имеют в имени класса "-value", 
+// начиная с class="budget_day-value" и заканчивая class="target_month-value">
+
+
+// Оставшиеся поля через querySelector каждый в отдельную переменную:
+
+// поля ввода (input) с левой стороны и не забудьте про range.
+
